3e8fea449531a984645ffba5f692ebeecbea1566
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2023-03-22 15:52:08 +0100

    add ParkingDynActivity

---
+++ src/main/java/org/matsim/parking/parkingsearch/DynAgent/ParkingDynActivity.java
@@ -0,0 +1,30 @@
+package org.matsim.parking.parkingsearch.DynAgent;
+
+import org.matsim.contrib.dynagent.DynActivity;
+
+public class ParkingDynActivity implements DynActivity{
+
+	public ParkingDynActivity (double now) {
+		
+	}
+	
+
+
+	@Override
+	public String getActivityType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getEndTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void doSimStep(double now) {
+		// TODO Auto-generated method stub
+		
+	}
+}
